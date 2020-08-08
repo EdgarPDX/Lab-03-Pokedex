0fa0cc8525adb1e716a8a03b9dacbe583853bc9c
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-08-07 19:45:54 -0700

    added pagination and css styles

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -3,20 +3,46 @@ import React, { Component } from 'react'
 
 export default class SearchInput extends Component {
     render() {
+        const {
+            totalPages,
+            handleNextClick,
+            handlePrevClick,
+            currentPage,
+            handleForm,
+            handleChange,
+            handleFilterChange,
+            componentDidMount 
+        } = this.props;
         return (
             <>
                 <div className="input">
-            <input placeholder="Pokemon Name" onChange ={this.props.handleChange} >
-          </input>
-          <select onChange={this.props.handleFilterChange}>
-              <option value='pokemon'>Name</option>
-              <option value='type'>Type</option>
-              <option value='attack'>Attack</option>
-              <option value='defense'>Defense</option>
-          </select>
-          <button onClick = {this.props.componentDidMount}>Who's that Pokemon</button>
-          
-          </div>
+                    <form onSubmit={handleForm}>
+                    <input placeholder="Pokemon Name" onChange ={handleChange} >
+                    </input>
+                    <select onChange={handleFilterChange}>
+                        <option value='pokemon'>Name</option>
+                        <option value='type'>Type</option>
+                        <option value='attack'>Attack</option>
+                        <option value='defense'>Defense</option>
+                    </select>
+                    <button onClick = {componentDidMount}>Who's that Pokemon</button>
+                    </form>
+                    
+                </div>
+                <div className="next-prev">
+                    {
+                        this.props.pokemon.length > 0 &&
+                        <div>
+                            { Number(currentPage) !== 1 && <button onClick ={handlePrevClick}>PREV PAGE</button>} 
+                            { Number(currentPage) !== Number(totalPages) && <button onClick = {handleNextClick}>NEXT PAGE</button>
+                            } <span>{currentPage} of {totalPages}</span>
+                        </div>
+                    }
+                </div>
+                    
+                    
+                    
+                
             </>
         )
     }
